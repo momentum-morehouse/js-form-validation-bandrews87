d854280769893663e424ae33e994e06e14b2240a
bandrews87 <70330631+bandrews87@users.noreply.github.com>
2020-09-22 15:03:55 -0500

    add event listener

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Add validation!');
+console.log("Add validation!");
+
+let form = document.querySelector("#parking-form");
+console.log(form);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("submitted");
+});
