ef6e117466cbcaecca114425dbf11df00e74d95e
bandrews87 <70330631+bandrews87@users.noreply.github.com>
2020-09-27 21:53:42 -0500

    form validation

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ let form = document.querySelector("#parking-form");
 console.log(form);
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  let total = document.querySelector("#total");
+  console.log(total);
+  let cost = document.createElement("p");
+  cost.textContent = "5";
+  console.log(cost);
+  console.log(total);
+  let numberofdays = 6;
+  let price = parseInt(cost.textContent) * numberofdays;
+  total.append(price);
   console.log("submitted");
 });
